1c38893c81f22348b5628af2a77b646d2e9cba19
PostScripton <postscripton.sp@gmail.com>
2022-10-19 22:58:19 +0300

    review: not to save timestamps manually

--- migrations/001_create_users_table.sql
+++ migrations/001_create_users_table.sql
@@ -1,8 +1,8 @@
 CREATE TABLE IF NOT EXISTS users
 (
-    id       SERIAL PRIMARY KEY,
-    login    varchar(255) NOT NULL UNIQUE,
-    password varchar(255) NOT NULL,
-    created_at timestamp,
-    updated_at timestamp
+    id         SERIAL PRIMARY KEY,
+    login      VARCHAR(255) NOT NULL UNIQUE,
+    password   VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT now(),
+    updated_at TIMESTAMP DEFAULT now()
 );
