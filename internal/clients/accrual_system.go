f42fa6007b9484d21df903c1fc3ec082e553ec7b
PostScripton <postscripton.sp@gmail.com>
2022-10-23 15:31:43 +0300

    feat: implement orders logic

---
+++ internal/clients/accrual_system.go
@@ -0,0 +1,84 @@
+package clients
+
+import (
+	"context"
+	"fmt"
+	"github.com/go-resty/resty/v2"
+	"github.com/rs/zerolog/log"
+	"net/http"
+	"time"
+)
+
+type Order struct {
+	Order   string   `json:"order"`
+	Status  string   `json:"status"`
+	Accrual *float64 `json:"accrual"`
+}
+
+type AccrualSystemClient struct {
+	client  *resty.Client
+	retryAt *time.Time
+}
+
+func NewAccrualSystemClient(baseURL string) *AccrualSystemClient {
+	return &AccrualSystemClient{
+		client: resty.New().SetBaseURL(baseURL),
+	}
+}
+
+func (asc *AccrualSystemClient) GetOrderInfo(ctx context.Context, number string) (*Order, error) {
+	order := new(Order)
+
+	resp, err := asc.client.R().
+		SetContext(ctx).
+		SetResult(order).
+		Get(fmt.Sprintf("/api/orders/%s", number))
+	if err != nil {
+		return nil, err
+	}
+	if err = asc.blocked(resp); err != nil {
+		return nil, err
+	}
+
+	log.Debug().
+		Int("code", resp.StatusCode()).
+		Str("status", resp.Status()).
+		Str("msg", string(resp.Body())).
+		Send()
+
+	if resp.StatusCode() == http.StatusNoContent {
+		return nil, nil
+	}
+
+	return order, nil
+}
+
+func (asc *AccrualSystemClient) CanMakeRequest() error {
+	if asc.retryAt == nil {
+		return nil
+	}
+
+	if time.Now().After(*asc.retryAt) {
+		asc.retryAt = nil
+		return nil
+	}
+
+	return fmt.Errorf("client unblocks in %s", time.Until(*asc.retryAt))
+}
+
+func (asc *AccrualSystemClient) blocked(resp *resty.Response) error {
+	if !resp.IsError() && resp.StatusCode() != http.StatusTooManyRequests {
+		return nil
+	}
+
+	retryAfter, err := time.ParseDuration(fmt.Sprintf("%ss", resp.Header().Get("Retry-After")))
+	if err != nil {
+		return err
+	}
+
+	body := string(resp.Body())
+	log.Debug().Str("resp", body).Msg("Client got blocked")
+	*asc.retryAt = time.Now().Add(retryAfter)
+
+	return fmt.Errorf(body)
+}
--- internal/models/user.go
+++ internal/models/user.go
@@ -3,9 +3,9 @@ package models
 import "time"
 
 type User struct {
-	ID        int       `json:"id"`
-	Login     string    `json:"login"`
-	Password  string    `json:"-"`
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
+	ID        int
+	Login     string
+	Password  string
+	CreatedAt time.Time
+	UpdatedAt time.Time
 }
